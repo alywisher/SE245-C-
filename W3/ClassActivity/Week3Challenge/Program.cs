4af048551712dc336741f0d86b6e21219a14645e
alywisher <alliewish101@gmail.com>
2024-04-09 08:59:27 -0400

    WTF

---
+++ W3/ClassActivity/Week3Challenge/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Week3Challenge -> create a multiplication table with nested loops from 1 to 10
+
+namespace Week3Challenge{
+    class Program{
+        static void Main (string[] args){
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(i);
+                
+                for (int j = 2; j <= 10; j++)
+                {
+                    Console.Write(j);
+                }
+            }
+        }
+    }
+}
+
+
+//Good crist what is happeing ??
+
+
