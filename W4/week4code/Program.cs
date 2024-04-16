1e153578ec4224ca710f6544a6b1ed4ca177ed7e
alywisher <alliewish101@gmail.com>
2024-04-16 09:12:00 -0400

    challenge complete

--- W4/week4code/Program.cs
+++ W4/week4code/Program.cs
@@ -1,7 +1,39 @@
 ﻿using System;
 
-class Program {
-    static void Main (string[] args) {
-        Console.WriteLine ("Hello World!");
+//create a progtam that includes point structure with integer X and Y coordinates, implement a method called GetRandomPoint that returns a pint with a specified range, call the method and display the random point
+
+namespace practice
+{
+
+    struct Point //where this bitch go
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
     }
+
+    class Program 
+    {
+        static Point GetRandomPoint()
+        {
+            Random rnd = new Random();
+            int X = rnd.Next(-100, 100);
+            int Y = rnd.Next(-100, 100);
+
+            return new Point(X, Y);
+        }
+        static void Main (string[] args) 
+        {
+            Point point = GetRandomPoint();
+
+            //display returned point
+            Console.Write($"Return Point: X={point.X}, Y={point.Y}");
+        }
+    }
+
 }
